3608ae985c1aee4a81b8550653f4bca2613e66cf
Niall Mullally <mullally@Nialls-MacBook-Pro.local>
2018-11-07 13:50:41 +0000

    Setup deleting items from leaderboard

--- Tapper/Leaderboard/Controller/PlayerInfoTableViewController.swift
+++ Tapper/Leaderboard/Controller/PlayerInfoTableViewController.swift
@@ -8,9 +8,8 @@
 
 import UIKit
 
-class PlayerInfoTableViewController: UITableViewController {
-
-    
+class PlayerInfoTableViewController: UITableViewController
+{    
     var tableData: [PlayerInfo] =
     [
         PlayerInfo(name: "Billy", score: 30324),
@@ -23,19 +22,53 @@ class PlayerInfoTableViewController: UITableViewController {
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let addItemButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        self.navigationItem.rightBarButtonItems = [editButtonItem, addItemButton]
+        
+        tableView.allowsMultipleSelectionDuringEditing = true
     }
     
-    @IBAction func addItem()
+    override func setEditing(_ editing: Bool, animated: Bool)
+    {
+        // we just hit the done button
+        // has to be called before super otherwise the selected items array is cleared
+        if editing == false
+        {
+            deleteItem(nil)
+        }
+        super.setEditing(editing, animated: animated)
+        tableView.setEditing(tableView.isEditing, animated: true)
+    }
+    
+    @objc func addItem()
     {
         let tempPlayer = PlayerInfo(name: "Temp", score: 0)
         tableData.append(tempPlayer)
         
-        tableView.reloadData()
+        // rather than reloading all the tableview data, we just call insert at the index of hte new item
+        let row = tableData.count - 1
+        let indexPath = IndexPath(row: row, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
 
+    @IBAction func deleteItem(_ sender: Any?)
+    {
+        if let selectedRows = tableView.indexPathsForSelectedRows
+        {
+            for indexPath in selectedRows
+            {
+                let row = indexPath.row > tableData.count - 1  ? tableData.count - 1 : indexPath.row
+//                let player = tableData[row]
+                tableData.remove(at: row)
+            }
+            
+            tableView.beginUpdates()
+            tableView.deleteRows(at: selectedRows, with: .automatic)
+            tableView.endUpdates()
+        }
+    }
+    
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tableData.count
@@ -51,25 +84,16 @@ class PlayerInfoTableViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            tableData.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
